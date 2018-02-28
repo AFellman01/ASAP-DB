04cc63952713172738392cefd858bdbed9274c00
Alexander Fellman <alexanderfellman@The-Wonderful-Wombat-of-Oz.local>
2018-02-28 13:09:11 -0500

    Framework

---
+++ schema.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS anomalyDB;
+create database anomalyDB;
+
+-- select * from ;
---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var db = require("./models");
+var app = express();
+var PORT = process.env.PORT || 8080;
+//var router = express.Router();
+// Sets up the Express app to handle data parsing
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "anomalyDB"
+});
+
+// var routes = require("./controllers/historyControllers.js");
+
+app.use("/", routes);
+
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  });
+});
